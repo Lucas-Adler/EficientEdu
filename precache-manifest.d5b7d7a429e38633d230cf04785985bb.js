1faa9a3f47e249325600e61bb645f53500398dfd
Lucas Adler <procheira.lucas@gmail.com>
2023-04-26 14:27:17 -0300

    Updates

---
+++ precache-manifest.d5b7d7a429e38633d230cf04785985bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f7c77046fdbd63ec28c",
+    "url": "/EficientEdu/static/css/main.c7fdc052.chunk.css"
+  },
+  {
+    "revision": "5f7c77046fdbd63ec28c",
+    "url": "/EficientEdu/static/js/main.c0fed393.chunk.js"
+  },
+  {
+    "revision": "8d055a45b541d4ace034",
+    "url": "/EficientEdu/static/js/runtime~main.eeb9e2b4.js"
+  },
+  {
+    "revision": "9b00de879415740165c8",
+    "url": "/EficientEdu/static/js/2.b187678d.chunk.js"
+  },
+  {
+    "revision": "a0983bcd65e96e532cf7",
+    "url": "/EficientEdu/static/js/3.1a378e0a.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/EficientEdu/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1ea13b51248552e48a29b1e04ee95870",
+    "url": "/EficientEdu/static/media/casa01.1ea13b51.png"
+  },
+  {
+    "revision": "caa170a93e4101d929b5587a81e05c88",
+    "url": "/EficientEdu/index.html"
+  }
+];
